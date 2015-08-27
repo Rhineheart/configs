e19e1234d482c096f449b8180175803170f506f6
Robin <rnswenson@gmail.com>
2015-08-27 18:16:18 +0200

    Wooooo

---
+++ copyConfigs.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+cp /home/rhine/.xmonad/xmonad.hs .
+cp /home/rhine/.rhine-zsh/themes/agnoster.zsh-theme .
+cp /home/rhine/.config/sublime-text-3/Packages/Colorsublime-Themes/Rhine.tmTheme .
+cp /home/rhine/.Xresources .
+cp /home/rhine/.zshrc .
